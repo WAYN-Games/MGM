725b8e98db97f1b5a36b05caf76b79b09eba65b7
WAYNGROUP <WAYNGROUP@users.noreply.github.com>
2019-06-05 14:13:48 +0200

    Move shot hit system from ray cast to trigger event.

--- Assets/MGM/Core/Systems/ShotHitSystem.cs
+++ Assets/MGM/Core/Systems/ShotHitSystem.cs
@@ -1,11 +1,8 @@
 ﻿using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
-using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Physics.Systems;
-using Unity.Transforms;
-using UnityEngine;
 
 namespace MGM.Core
 {
@@ -13,128 +10,69 @@ namespace MGM.Core
     public class DestroyOnCollisionSystem : JobComponentSystem
     {
         BeginInitializationEntityCommandBufferSystem m_EntityCommandBufferSystem;
+        BuildPhysicsWorld m_BuildPhysicsWorldSystem;
+        StepPhysicsWorld m_StepPhysicsWorldSystem;
 
+        EntityQuery JobGroup;
 
-        struct DealDamageTo
-        {
-            public Entity Entity;
-            public int Amount;
-        }
 
         protected override void OnCreate()
         {
             m_EntityCommandBufferSystem = World.GetOrCreateSystem<BeginInitializationEntityCommandBufferSystem>();
+ 
+            m_BuildPhysicsWorldSystem = World.GetOrCreateSystem<BuildPhysicsWorld>();
+            m_StepPhysicsWorldSystem = World.GetOrCreateSystem<StepPhysicsWorld>();
+            JobGroup = GetEntityQuery(new EntityQueryDesc
+            {
+                All = new ComponentType[] { typeof(Damage) }
+            });
         }
 
-        [RequireComponentTag(typeof(DestroyOnColision))]
-        struct DetectCollisionJob : IJobForEachWithEntity<Speed, Damage, LocalToWorld>
+        struct ProjectileDamageJob : ITriggerEventsJob
         {
-            [WriteOnly] public NativeQueue<Entity>.Concurrent BulletsThatCollided;
-            [WriteOnly] public NativeQueue<DealDamageTo>.Concurrent EntitiesHitByBullet;
-            [ReadOnly] public float DeltaTime;
-            [ReadOnly] public CollisionWorld World;
+            [ReadOnly] public ComponentDataFromEntity<Damage> DamageDealer;
+            public ComponentDataFromEntity<Health> Target;
+            public EntityCommandBuffer CommandBuffer;
 
-            public void Execute(Entity entity, int index, [ReadOnly] ref Speed speed, [ReadOnly] ref Damage damage,
-                [ReadOnly] ref LocalToWorld localToWorld)
+            public void Execute(TriggerEvent triggerEvent)
             {
-                var RaycastInput = new RaycastInput
-                {
-                    Start = localToWorld.Position,
-                    End = localToWorld.Position + localToWorld.Forward * DeltaTime * speed.Value,
-                    Filter = CollisionFilter.Default
-                };
 
+                Entity entityA = triggerEvent.Entities.EntityA;
+                Entity entityB = triggerEvent.Entities.EntityB;
 
-                if (World.CastRay(RaycastInput,out Unity.Physics.RaycastHit hit))
-                {
-                    BulletsThatCollided.Enqueue(entity);
-                    DealDamageTo ddt = new DealDamageTo()
-                    {
-                        Entity = World.Bodies[hit.RigidBodyIndex].Entity,
-                        Amount = damage.Value
-                    };
-                    EntitiesHitByBullet.Enqueue(ddt);
-                }
+                bool isBodyAProjectile = DamageDealer.Exists(entityA);
+                bool isBodyBProjectile = DamageDealer.Exists(entityB);
 
+                // Ignoring Triggers overlapping other Triggers
+                if (isBodyAProjectile && isBodyBProjectile)
+                    return;
+
+                var projectileEntity = isBodyAProjectile ? entityA : entityB;
+                var targetEntity = isBodyAProjectile ? entityB : entityA;
+
+                // destroy bullet.
+                CommandBuffer.DestroyEntity(projectileEntity);
+
+                if (Target.Exists(targetEntity)) { 
+
+                // Decrease target HP
+                var hp = Target[targetEntity];
+                hp.Value -= DamageDealer[projectileEntity].Value;
+                Target[targetEntity] = hp;
             }
-
-
-        }
-
-        struct ApplyDamage : IJobForEachWithEntity<Health>
-        {
-            public EntityCommandBuffer.Concurrent CommandBuffer;
-            [ReadOnly] public NativeQueue<DealDamageTo> DamageToApplyTo;
-
-            public void Execute(Entity entity, int index, ref Health hp)
-            {
-                for (int i = 0; i < DamageToApplyTo.Count; i++)
-                {
-                   DealDamageTo ddt = DamageToApplyTo.Peek();
-                   if (ddt.Entity.Equals(entity))
-                    {
-                        hp.Value -= ddt.Amount;                        
-                    }
-                }
-            }
-        }
-
-        struct DestroyOnImapct : IJobParallelFor
-        {
-            public EntityCommandBuffer.Concurrent CommandBuffer;
-            [ReadOnly] public NativeQueue<Entity> EntitiesToDestroy;
-
-            public void Execute(int index)
-            {
-               CommandBuffer.DestroyEntity(index, EntitiesToDestroy.Peek());
             }
         }
 
         protected override JobHandle OnUpdate(JobHandle inputDeps)
         {
-       
-            NativeQueue<Entity> BulletsThatCollided = new NativeQueue<Entity>(Allocator.TempJob);
-            NativeQueue<DealDamageTo> DamageDoneToEntitiesHitByBullet = new NativeQueue<DealDamageTo>(Allocator.TempJob);
-
-            var detectionJob = new DetectCollisionJob
+            return  new ProjectileDamageJob
             {
-                DeltaTime = Time.deltaTime,
-                BulletsThatCollided = BulletsThatCollided.ToConcurrent(),
-                EntitiesHitByBullet = DamageDoneToEntitiesHitByBullet.ToConcurrent(),
-                World = World.Active.GetExistingSystem<BuildPhysicsWorld>().PhysicsWorld.CollisionWorld
-            }.Schedule(this, inputDeps);
+                    CommandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer(),
+                    DamageDealer = GetComponentDataFromEntity<Damage>(true),
+                    Target = GetComponentDataFromEntity<Health>()
+            }.Schedule(m_StepPhysicsWorldSystem.Simulation,
+                      ref m_BuildPhysicsWorldSystem.PhysicsWorld, inputDeps);
 
-            detectionJob.Complete();
-
-            
-            var destroyBulletJob = new DestroyOnImapct
-            {
-                CommandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent(),
-                EntitiesToDestroy = BulletsThatCollided
-            }.Schedule(BulletsThatCollided.Count, 64 ,detectionJob);
-            /*
-            m_EntityCommandBufferSystem.AddJobHandleForProducer(destroyBulletJob);
-            */
-            destroyBulletJob.Complete();
-
-            // Temp fix because Collection support for paralelism is to weak now. Lose perf but at least it works as expected.
-            while (BulletsThatCollided.Count != 0)
-            {
-                EntityManager.DestroyEntity(BulletsThatCollided.Dequeue());
-            }
-
-            var applyDamageJob = new ApplyDamage
-            {
-                CommandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent(),
-                DamageToApplyTo = DamageDoneToEntitiesHitByBullet
-            }.Schedule(this, destroyBulletJob);
-
-            applyDamageJob.Complete();
-
-            BulletsThatCollided.Dispose();
-            DamageDoneToEntitiesHitByBullet.Dispose();
-
-            return applyDamageJob;
         }
     }
 }
