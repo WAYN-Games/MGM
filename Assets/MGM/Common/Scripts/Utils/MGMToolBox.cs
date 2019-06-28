7c3e475a164a250872d898b5dbf08da7764910b9
WAYN <wayn.group@outlook.com>
2019-06-28 19:43:41 +0200

    revert

---
+++ Assets/MGM/Common/Scripts/Utils/MGMToolBox.cs
@@ -0,0 +1,31 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Physics.Systems;
+using UnityEngine;
+namespace MGM.Common
+{
+    public class MGMToolBox
+    {
+        public static float3 ECS_MouseToWorldPosition(Vector2 mousePos, Camera cam)
+        {
+
+            RaycastInput RaycastInput = new RaycastInput
+            {
+                Start = cam.transform.position,
+                End = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.farClipPlane)),
+
+                Filter = CollisionFilter.Default
+            };
+
+            CollisionWorld w = World.Active.GetExistingSystem<BuildPhysicsWorld>().PhysicsWorld.CollisionWorld;
+
+            Unity.Physics.RaycastHit hit = new Unity.Physics.RaycastHit();
+
+            RayCastJobUtils.SingleRayCast(w, RaycastInput, ref hit);
+
+            return hit.Fraction == 0 ? RaycastInput.End : hit.Position;
+
+        }
+    }
+}
--- Assets/MGM/Core/InputResponses/MousePositionResponse.cs
+++ Assets/MGM/Core/InputResponses/MousePositionResponse.cs
@@ -1,6 +1,7 @@
 ﻿using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Physics;
-using Unity.Physics.Systems;
+using MGM.Common;
 using UnityEngine;
 using UnityEngine.InputSystem;
 namespace MGM
@@ -10,34 +11,19 @@ namespace MGM
 
         protected override void RespondToAction(InputAction.CallbackContext context)
         {
-            
             Vector2 mousePos = context.ReadValue<Vector2>();
-            
+
             Camera cam = Camera.main;
-                        
-            RaycastInput RaycastInput = new RaycastInput
-            {
-                Start = cam.transform.position,
-                End = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.farClipPlane)),          
-            
-                Filter = CollisionFilter.Default
-            }; 
-
-            CollisionWorld w = World.Active.GetExistingSystem<BuildPhysicsWorld>().PhysicsWorld.CollisionWorld;
-
-            Unity.Physics.RaycastHit hit = new Unity.Physics.RaycastHit(); 
-            
-            RayCastJobUtils.SingleRayCast(w, RaycastInput, ref hit);
-
-
 
             Aim aim = B_EntityManager.GetComponentData<Aim>(B_Entity);
 
-            var position = hit.Fraction == 0 ? RaycastInput.End: hit.Position;
+            aim.Value = MGMToolBox.ECS_MouseToWorldPosition(mousePos, cam);
 
-            aim.Value = position;
             B_EntityManager.SetComponentData(B_Entity, aim);
-
         }
+
+
+
+
     }
 }
