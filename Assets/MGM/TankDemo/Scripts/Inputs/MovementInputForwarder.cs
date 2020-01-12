207cb0993a1483887d3dfcb7459bc7ef842c0601
WAYNGROUP <WAYNGROUP@users.noreply.github.com>
2020-01-12 21:09:13 +0100

    fix building collider, fix relative movement

--- Assets/MGM/TankDemo/Scripts/Inputs/MovementInputForwarder.cs
+++ Assets/MGM/TankDemo/Scripts/Inputs/MovementInputForwarder.cs
@@ -4,9 +4,30 @@ using UnityEngine.InputSystem;
 
 public class MovementInputForwarder: InputActionForwarder<MovementDirection>
 {
+    public Camera Camera;
+
+    private void Awake()
+    {
+        if(Camera == null)
+        {
+            Camera = Camera.main;
+        }
+    }
+
     public override void ReadAction(InputAction.CallbackContext ctx)
     {
         Vector2 InputMouvementDirection = ctx.ReadValue<Vector2>();
-        ForwardAction(new MovementDirection() { Value = new float3(InputMouvementDirection.x, 0, InputMouvementDirection.y) });
+        //camera forward and right vectors:
+        var forward = Camera.transform.forward;
+        var right = Camera.transform.right;
+
+        //project forward and right vectors on the horizontal plane (y = 0)
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+        //this is the direction in the world space we want to move:
+        var desiredMoveDirection = forward * InputMouvementDirection.y + right * InputMouvementDirection.x;
+        ForwardAction(new MovementDirection() { Value = desiredMoveDirection });
     }
 }
